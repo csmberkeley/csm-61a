d2a8f992d779f20712413cd75ab41f4883726c8c
Ethan Chiu <ethanchiu@berkeley.edu>
2020-09-24 13:31:43 -0700

    Add binary problem, review problems

--- src/fa20/mentor07_sol.py
+++ src/fa20/mentor07_sol.py
@@ -1,6 +1,5 @@
->>> a = [1, 2]
->>> b = a
->>> print(a.append([3, 4]))
+
+
 None
 >>> a
 [1, 2, [3, 4]]
@@ -36,5 +35,36 @@ def accumulate(lst):
             sum_so_far += item
             lst[i] = sum_so_far
     return sum_so_far
+
+
+def collapse(n):
+    left, last = n // 10, n % 10
+    if left == 0:
+        return last
+    elif last == left % 10:
+        return collapse(left)
+    else:
+        return collapse(left) * 10 + last
+
+
+def make_change(n):
+    if n == 0:
+        return 0
+    elif n < 3:
+        return 1 + make_change(n - 1)
+    elif n < 4:
+        return 1 + min(make_change(n - 1), make_change(n - 3))
+    else:
+        return 1 + min(make_change(n - 1), make_change(n - 3), make_change(n - 4))
+
+
+def max_subset_sum(lst, n):
+    if n == 0:
+        return 0
+    elif len(lst) <= n:
+        return sum(lst)
+    with_elem = max_subset_sum(lst[1:], n - 1) + lst[0]
+    without_elem = max_subset_sum(lst[1:], n)
+    return max(with_elem, without_elem)
 
 
