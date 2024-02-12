fb7bc8fe949b6189b56ba0a813d716d45bcf896e
alyssa smith <alyssaisridingonrainbowturtles@gmail.com>
2024-02-12 13:08:41 -0800

    rework of mentor05

---
+++ topics/recursion/text/example.py
@@ -0,0 +1,12 @@
+def count_partitions(n, m):
+        """Count the ways to partition n using parts up to m."""
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 0
+        elif m == 0:
+            return 0
+        else:
+            return count_partitions(n-m, m) + count_partitions(n, m-1)
+
+print(count_partitions(4, 4))
