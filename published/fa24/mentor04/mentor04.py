cd1729a17f7d711ff9d6fbae3a0fc5e285e11d86
bryanvent24 <86426182+bryanvent24@users.noreply.github.com>
2024-09-14 19:12:11 -0700

    mentor04 (#208)
    * mentor04 before review
    
    * Mentor04, added note for page 5 (extra scratch paper)
    
    * Added files to published folder fa24

---
+++ published/fa24/mentor04/mentor04.py
@@ -0,0 +1,63 @@
+def dream1(f):
+    def dream2(secret):
+        mind = f(secret)
+        kick = lambda x: mind()
+        return kick(2)
+    return dream2
+
+inception = lambda secret: lambda: secret
+real = dream1(inception)(42)
+
+
+def a(y):
+    d = 1
+    b = lambda x: y(x)
+    e = lambda x: x(3)
+    return e(b)
+d = 5
+a(lambda x: 4 - x + d)
+
+
+def compound(base_func, prev_compound=lambda x: x):
+    """
+    >>> add_one = lambda x: x + 1
+    >>> adder = compound(add_one)
+    >>> adder = adder(3, 2)
+    3       # 3
+    4       # f(3)
+    >>> adder = adder(4, 4)
+    6       # f(f(4))
+    7       # f(f(f(4)))
+    8       # f(f(f(f(4))))
+    9       # f(f(f(f(f(4)))))
+    """
+    def g(x, n):
+      new_comp = ____________________________
+      while n > 0:
+        print(____________________________)
+        new_comp = (lambda save_comp: \
+                    _______________________)(____________)
+        ___________________________________________________
+      return ______________________________________________
+    return ___________________________________  
+
+
+def factorial(n):
+    return n * factorial(n)
+
+
+def num_digits(n):
+    """Takes in an positive integer and returns the number of
+    digits.
+
+    >>> num_digits(0)
+    1
+    >>> num_digits(1)
+    1
+    >>> num_digits(7)
+    1
+    >>> num_digits(1093)
+    4
+    """
+
+
