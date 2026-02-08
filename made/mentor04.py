9488e4dbac0ede17523ac6d17a828bb5bfc40db6
Tavisha Patel <tavisha_patel@berkeley.edu>
2026-02-07 21:19:06 -0800

    Finished mentor 04

---
+++ made/mentor04.py
@@ -0,0 +1,90 @@
+def a(y):
+    d = 1
+    b = lambda x: y(x)
+    e = lambda x: x(3)
+    return e(b)
+d = 5
+a(lambda x: 4 - x + d)
+
+
+def boba_line(n):
+    """Takes in an integer of 0s and 1s and returns the number of 1s.
+
+    >>> boba_line(0)
+    0
+    >>> boba_line(1)
+    1
+    >>> boba_line(10101)
+    3
+    >>> boba_line(10001)
+    2
+    """
+    if __________________ :             # base case
+
+        return _________________
+
+    else:
+
+        return ________________  +  ____________________   # recursive case
+
+
+
+def gib(n):
+    """
+    >>> gib(0)
+    0
+    >>> gib(1)
+    1
+    >>> gib(2)
+    2
+    >>> gib(3) # gib(2) + gib(1) + gib(0) = 3
+    3
+    >>> gib(4) # gib(3) + gib(2) + gib(1) = 6
+    6
+    """
+    if ______________________________:
+
+        return ______________________________
+        
+    return ______________________________
+
+
+def num_digits(n):
+    """Takes in an positive integer and returns the number of
+    digits.
+
+    >>> num_digits(0)
+    1
+    >>> num_digits(1)
+    1
+    >>> num_digits(7)
+    1
+    >>> num_digits(1093)
+    4
+    """
+
+
+def fizzbuzz(n):
+    """Prints the numbers from 1 to n. If the number is divisible by 3, it
+    instead prints 'fizz'. If the number is divisible by 5, it instead prints
+    'buzz'. If the number is divisible by both, it prints 'fizzbuzz'. You must do this recursively!
+
+    >>> fizzbuzz(15)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    """
+
+
---
+++ made/mentor04_sol.py
@@ -0,0 +1,76 @@
+def a(y):
+    d = 1
+    b = lambda x: y(x)
+    e = lambda x: x(3)
+    return e(b)
+d = 5
+a(lambda x: 4 - x + d)
+
+
+    if n < 10:
+        return n
+    else:
+        return (n % 10) + boba_line(n // 10)
+
+
+def gib(n):
+    if n <= 2:
+        return n
+    return gib(n - 1) + gib(n - 2) + gib(n - 3)
+
+
+def num_digits(n):
+    """Takes in an positive integer and returns the number of
+    digits.
+
+    >>> num_digits(0)
+    1
+    >>> num_digits(1)
+    1
+    >>> num_digits(7)
+    1
+    >>> num_digits(1093)
+    4
+    """
+    if n < 10:
+        return 1
+    else:
+        return 1 + num_digits(n // 10)
+
+
+def fizzbuzz(n):
+    """Prints the numbers from 1 to n. If the number is divisible by 3, it
+    instead prints 'fizz'. If the number is divisible by 5, it instead prints
+    'buzz'. If the number is divisible by both, it prints 'fizzbuzz'. You must do this recursively!
+
+    >>> fizzbuzz(15)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    """
+    if n == 1:
+        print(n)
+    else:
+        fizzbuzz(n - 1)
+        if n % 3 == 0 and n % 5 == 0:
+            print('fizzbuzz')
+        elif n % 3 == 0:
+            print('fizz')
+        elif n % 5 == 0:
+            print('buzz')
+        else:
+            print(n)
+
+
