a5e100530370457d2045b4fdede4e8c87210d09a
Rajoshi Basu <34625067+R1A2J@users.noreply.github.com>
2026-09-01 19:19:43 -0700

    done with fa26/mentor02 (#240)

---
+++ made/mentor02.py
@@ -0,0 +1,112 @@
+>>> 3
+>>> "cs61a"
+>>> x = 3
+>>> x
+>>> x = print("cs61a")
+cs61a
+>>> x
+>>> print(print(print("cs61a")))
+>>> def f1(x):
+...	return x + 1
+>>> f1(3)
+>>> f1(2) + f1(2 + 3)
+>>> def f2(y):
+...	return y / 0
+>>> f2(4)
+>>> def f3(x, y):
+...	if x > y:
+...		return x
+...	elif x == y:
+...		return x + y
+...	else:
+...		return y
+>>> f3(1, 2)
+>>> f3(5, 5)
+>>> 1 or 2 or 3
+>>> 1 or 0 or 3
+>>> 4 and (2 or 1/0)
+>>> 0 or (not 1 and 3)
+>>> (2 or 1/0) and (False or (True and (0 or 1)))
+
+
+add(1, mul(2, 3))
+add(mul(2, 3), add(1, 4))
+max(mul(1, 2), add(5, 6), 3, mul(mul(3, 4), 1), 7)
+
+
+def score_needed(current_grade, num_exams, pass_grade):
+    '''
+    >>> score_needed(89, 4, 90)
+    94
+    >>> score_needed(65, 2, 70)
+    80
+    >>> score_needed(77, 10, 78)
+    88
+    '''
+    
+
+
+
+def swap(x, y):
+    x, y = y, x
+    return print("Swapped!", x, y)
+
+x, y = 60, 1
+a = swap(x, y)
+swap(a, y)
+
+
+
+def divisibility_check(num):
+
+
+
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> result is None
+    True
+    """
+
+
+def whole_sum(n): 
+    """ 
+    >>> whole_sum(21)(777)
+    True
+    >>> whole_sum(142)(10010101010)
+    False
+    """
+    def check(x):
+
+        ___________________
+		
+        while _____________:
+		
+            last = __________________
+				
+            _________________________
+				
+            _________________________
+				
+        return __________________
+		
+    return _________________
+
+
+
